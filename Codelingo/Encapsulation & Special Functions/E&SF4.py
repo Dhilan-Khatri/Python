8b1796c8d34809409b8d3dad2d2becaafcf32d67
DhilanKhatriFTC <khatridhilan@gmail.com>
2025-11-06 19:07:04 -0500

    save

---
+++ Codelingo/Encapsulation & Special Functions/E&SF4.py
@@ -0,0 +1,11 @@
+class reverse:
+    def __init__(self, s=""):
+        self.s = s
+
+    def reversed_string(self):
+        return self.s[::-1]
+
+word = input("Enter a word: ")
+rev = reverse(word)
+
+print("Reversed string:", rev.reversed_string())
