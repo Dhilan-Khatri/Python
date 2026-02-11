12f864af62aa0e5da01878e434751a585977d19f
DhilanKhatriFTC <khatridhilan@gmail.com>
2026-02-10 20:23:08 -0500

    Delete Codelingo/CV1.py

--- Codelingo/CV1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-image=cv2.imread("download.jfif")
-cv2.namedWindow("loadedImage", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("loadedImage", 800,500)
-cv2.imshow("loadedImage", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print(f"Image Dimension {image.shape}")
