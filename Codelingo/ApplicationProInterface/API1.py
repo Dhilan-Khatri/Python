07814adcf28186de5aa94f22c0899884f8f4c1c1
DhilanKhatriFTC <khatridhilan@gmail.com>
2026-04-07 20:26:16 -0400

    save

--- Codelingo/ApplicationProInterface/API1.py
+++ Codelingo/ApplicationProInterface/API1.py
@@ -2,18 +2,18 @@ import requests
 from colorama import init, Fore
 init(autoreset=True)
 def getRandomJokes():
-    site="https://official-joke-api.appspot.com/random_joke"
+    site="https://catfact.ninja/fact"
     response=requests.get(site)
     if response.status_code==200:
         print(Fore.LIGHTMAGENTA_EX+f"Full JSON Responce: {response.json()}")
         jokeData=response.json()
-        return f"{jokeData['setup']} - {jokeData['punchline']}"
+        return f"{jokeData['fact']}"
     else:
         return Fore.RED+"Failed to Fetch Joke"
 name=input(Fore.CYAN+"Welcome, What is Your Name? ")
-print(Fore.CYAN+f"Welcome {name} to the Random Joke Generator")
+print(Fore.CYAN+f"Welcome {name} to the Random Cat Fact Generator")
 while True:
-    userInput=input(Fore.BLACK+"Press Enter to Get A New Joke\nOr press 'q' to exit: ").strip().lower()
+    userInput=input(Fore.BLACK+"Press Enter to Get A New Fact\nOr press 'q' to exit: ").strip().lower()
     if userInput=="q":
         print(Fore.CYAN+f"Goodbye {name}, See You Again Soon")
         break
